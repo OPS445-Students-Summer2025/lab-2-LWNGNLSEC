7f05589cb1c4f45235cd3b55bbdad05f7a3d1438
Chanho Lee <clee259@myseneca.ca>
2025-05-13 14:23:39 -0400

    Added lab2a1

---
+++ lab2a1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+name = 'Jon'
+age = 20
+
+#print('Hi ' + name + ', you are ' + str(age) + ' years old.') '''commented out'''
+
+colour = input("Type in a colour and press enter: ") #'''nothing should be printed'''
+
+print('The colour I typed in is: ' + colour) #'''expected outcome'''
+
+
