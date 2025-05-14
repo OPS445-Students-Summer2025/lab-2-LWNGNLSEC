34d2f6d0c3c4b849310e37f8cc0909064f818a11
Chanho Lee <clee259@myseneca.ca>
2025-05-13 20:19:52 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Author: Chanho Lee
+# Author ID: clee259
+# Date Created: 2025/05/13 GMT-4 20:20
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print("blast off!")
