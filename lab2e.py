d7c3af75b8a02773b113e3e5b9641de32c487b34
Chanho Lee <clee259@myseneca.ca>
2025-05-13 20:13:48 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Author: Chanho Lee
+# Author ID: clee259
+# Date Created: 2025/05/13 GMT-4 20:09
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print("blast off!")
